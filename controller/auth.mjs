cd6f605c6d6154412014387e949df063c1885b9f
Hamza Yousaf <hamzay050@gmail.com>
2023-06-23 01:47:26 +0500

    change password implemented

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -118,4 +118,54 @@ async function resetPassword(req, res) {
     return httpError('Error occur while updating password')
   }
 }
-export { signup, login, forgotPassword, resetPassword }
+
+
+const changePassword = async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+   const userId = req.user.userId;  
+
+  try {
+    // Find the user by ID
+     const currentUser = await user.findOne({ where: { id: userId } });
+     if (!currentUser) {
+      return res.status(404).json({ message: 'currentUser not found' });
+    }
+
+    // Verify the current password
+    const isMatch = await bcrypt.compare(currentPassword, currentUser.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid current password' });
+    }
+ 
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+ 
+    // Update the currentUser's password
+const a =await user.update(
+      { password: hashedPassword },
+      { where: { id: userId } }
+    )
+    // Generate a new JWT token with updated user data
+   const   userData = {
+      userId: req.user.userId,
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+      email: req.user.email,
+      admin: req.user.isAdmin,
+    }
+        const token = await accessToken(userData)
+    res.cookie('accessToken', token)
+
+    return res.success({
+      status: 200,
+      message: 'Successfully Change Password!',
+      data: userData
+    })
+ 
+    
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while changing the password', error });
+  }
+};
+
+export { signup, login, forgotPassword, resetPassword,changePassword }
--- routes/auth.mjs
+++ routes/auth.mjs
@@ -4,12 +4,16 @@ import {
   signup,
   login,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  changePassword
 } from '../controller/auth.mjs'
+import verifyAccessToken from '../middleware/verifyAccessToken .mjs'
 
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/forgotPassword', forgotPassword)
 router.post('/resetPassword', resetPassword)
+router.put('/changePassword',verifyAccessToken, changePassword)
+
 
 export default router
